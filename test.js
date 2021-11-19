f7d9d8d7305c4921df43c6cb72cff0569fee2fe9
Naomi Calabretta <me@arytonex.pw>
2021-11-19 16:42:07 +0100

    add tests

---
+++ test.js
@@ -0,0 +1,14 @@
+const Player = require("./");
+
+let player;
+
+function onUpdate(){
+    const update = player.getUpdate();
+    console.log(update);
+}
+
+player = new Player(onUpdate);
+
+(async()=>{
+    while(1) await new Promise(r => setTimeout(r, 10 * 1000));
+})();
