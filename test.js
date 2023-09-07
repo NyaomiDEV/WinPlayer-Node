53b739cbd338c0c101a584e6302692b21346fd94
Naomi Calabretta <nyaomicalabretta@gmail.com>
2023-09-07 18:55:33 +0200

    ffs async await

--- test.js
+++ test.js
@@ -1,39 +1,43 @@
 const winPlayer = require(".");
 
-const playerManager = await winPlayer.getPlayerManager();
-let player;
+async function main(){
+	const playerManager = await winPlayer.getPlayerManager();
+	let player;
 
-async function pmEventPolling(){
-	while(evt = await playerManager.pollNextEvent()) {
-		console.log("manager event", evt);
-		switch(evt){
-			case "CurrentSessionUpdated":
-				playerManager.updateSystemSession();
-				break;
-			case "SessionsUpdated":
-				playerManager.updateSessions(null);
-				player = playerManager.getActiveSession();
-				if (!player)
-					console.log("manager: WTF?!");
-				else
-					eventPolling();
-				break;
+	async function pmEventPolling(){
+		while(evt = await playerManager.pollNextEvent()) {
+			console.log("manager event", evt);
+			switch(evt){
+				case "CurrentSessionUpdated":
+					playerManager.updateSystemSession();
+					break;
+				case "SessionsUpdated":
+					playerManager.updateSessions(null);
+					player = playerManager.getActiveSession();
+					if (!player)
+						console.log("manager: WTF?!");
+					else
+						eventPolling();
+					break;
+			}
 		}
 	}
-}
 
-async function eventPolling() {
-	while (evt = await player.pollNextEvent()) {
-		console.log("manager event", evt);
-		switch (evt) {
-			case "PlaybackInfoChanged":
-				console.log(await player.getStatus())
-				break;
-			case "TimelinePropertiesChanged":
-				console.log(await player.getPosition(false))
-				break;
+	async function eventPolling() {
+		while (evt = await player.pollNextEvent()) {
+			console.log("manager event", evt);
+			switch (evt) {
+				case "PlaybackInfoChanged":
+					console.log(await player.getStatus())
+					break;
+				case "TimelinePropertiesChanged":
+					console.log(await player.getPosition(false))
+					break;
+			}
 		}
 	}
+
+	pmEventPolling()
 }
 
-pmEventPolling()
+main();
