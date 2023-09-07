5807f461eef1f4d693e2704ee1639372d7f936f1
Naomi Calabretta <nyaomicalabretta@gmail.com>
2023-09-07 18:54:08 +0200

    do a test js function

---
+++ test.js
@@ -0,0 +1,39 @@
+const winPlayer = require("");
+
+const playerManager = await winPlayer.getPlayerManager();
+let player;
+
+async function pmEventPolling(){
+	while(evt = await playerManager.pollNextEvent()) {
+		console.log("manager event", evt);
+		switch(evt){
+			case "CurrentSessionUpdated":
+				playerManager.updateSystemSession();
+				break;
+			case "SessionsUpdated":
+				playerManager.updateSessions(null);
+				player = playerManager.getActiveSession();
+				if (!player)
+					console.log("manager: WTF?!");
+				else
+					eventPolling();
+				break;
+		}
+	}
+}
+
+async function eventPolling() {
+	while (evt = await player.pollNextEvent()) {
+		console.log("manager event", evt);
+		switch (evt) {
+			case "PlaybackInfoChanged":
+				console.log(await player.getStatus())
+				break;
+			case "TimelinePropertiesChanged":
+				console.log(await player.getPosition(false))
+				break;
+		}
+	}
+}
+
+pmEventPolling()
