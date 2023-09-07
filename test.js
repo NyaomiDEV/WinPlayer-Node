6393e0a162103ae947e0532647143f05daf80a43
Naomi Calabretta <nyaomicalabretta@gmail.com>
2023-09-07 19:03:41 +0200

    fixup test js

--- test.js
+++ test.js
@@ -1,23 +1,25 @@
 const winPlayer = require(".");
 
-async function main(){
+async function main() {
 	const playerManager = await winPlayer.getPlayerManager();
 	let player;
 
-	async function pmEventPolling(){
-		while(evt = await playerManager.pollNextEvent()) {
+	async function pmEventPolling() {
+		while (evt = await playerManager.pollNextEvent()) {
 			console.log("manager event", evt);
-			switch(evt){
-				case "CurrentSessionUpdated":
+			switch (evt) {
+				case "CurrentSessionChanged":
 					playerManager.updateSystemSession();
 					break;
-				case "SessionsUpdated":
+				case "SessionsChanged":
 					playerManager.updateSessions(null);
-					player = playerManager.getActiveSession();
+					player = await playerManager.getActiveSession();
 					if (!player)
 						console.log("manager: WTF?!");
-					else
+					else {
+						console.log("player attached:", await player.getAumid());
 						eventPolling();
+					}
 					break;
 			}
 		}
@@ -31,7 +33,7 @@ async function main(){
 					console.log(await player.getStatus())
 					break;
 				case "TimelinePropertiesChanged":
-					console.log(await player.getPosition(false))
+					console.log(await player.getStatus(), await player.getPosition(false))
 					break;
 			}
 		}
